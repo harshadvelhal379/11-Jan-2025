b757b76cf1886723ee281a29afe26e6017b99fb4
Harshad Velhal <harshadvelhal379@gmail.com>
2025-01-11 19:06:05 +0530

    another message

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ setTimeout(() => {
     console.log(a);
     
 }, 1000);
-
+git= function harshad(a,b) {
+    setTimeout(() => {
+        console.log("vs code bhi chutiya hai");
+    }, 2000);
+  
+}
 
 
 let b=30
 
 
 let x= "harshad "
+let df= "chutiya hai bc github"
